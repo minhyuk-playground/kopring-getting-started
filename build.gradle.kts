446cedd32febad1094196a792732a39fc7d6483b
minhyeok yoon <ymh92730@gmail.com>
2022-10-27 19:04:03 +0900

    feat: init commit

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+//    id("org.asciidoctor.convert") version "1.5.8" => Gradle7 에서 사용 못함
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+    kotlin("jvm") version "1.6.21"
+    kotlin("kapt") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
+}
+
+group = "com.musinsa"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+val asciidoctor: Configuration by configurations.creating
+
+repositories {
+    mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+    // DB
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("com.querydsl:querydsl-jpa")
+    kapt("com.querydsl:querydsl-apt:${dependencyManagement.importedProperties["querydsl.version"]}:jpa")
+
+    // api docs
+    asciidoctor("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+    // ETC
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+        outputs.dir(project.extra["snippetsDir"]!!)
+    }
+
+    asciidoctor {
+        configurations(asciidoctor.name)
+        inputs.dir(project.extra["snippetsDir"]!!)
+        dependsOn(test)
+    }
+
+    bootJar {
+        dependsOn(asciidoctor)
+        from("${asciidoctor.get().outputDir}") {
+            into("static/docs")
+        }
+    }
+}
---
+++ src/main/kotlin/com/musinsa/board/BoardApplication.kt
@@ -0,0 +1,11 @@
+package com.musinsa.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringGettingStartedApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringGettingStartedApplication>(*args)
+}
